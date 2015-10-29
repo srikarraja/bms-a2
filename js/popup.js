e283e27b05420683c435e84e4eb771bd78d62004
srikarraja <rajasrikar2010@gmail.com>
2015-10-30 00:53:26 +0530

    Chrome Extension
    It works...

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,15 @@
 
 window.onload = function(){
-  document.getElementById('searchText').onkeypress = searchText;
+  document.getElementById('searchBtn').onclick = searchText;
 };
 
 function searchText(){
     var search = document.getElementById('searchText').value;
     if (search) {
 chrome.tabs.query({active: true, lastFocusedWindow: true }, function (tabs) {
-alert("Data Received");      
+   
       chrome.tabs.executeScript(tabs[0].id, {file: 'js/search.js' }, function() {
-alert("Data Sending...!!");
-  
+
                 chrome.tabs.sendMessage(tabs[0].id, {method: 'search',searchText: search});
             });
         });
--- js/search.js
+++ js/search.js
@@ -1,23 +1,28 @@
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
   // message.searchText is the text that was captured in the popup    
   // Search/Highlight code goes here
+ var text=message.searchText; 
 
-alert(searchText);
-function search(searchTerm, selector) {
-    if(searchTerm) {        
-        var selector = selector || "body";                             //use body as selector if none provided
-        var searchTermRegEx = new RegExp(searchTerm,"ig");
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.type = 'text/css';
+style.href = chrome.extension.getURL('highlight.css');
+(document.head||document.documentElement).appendChild(style);
+    
+
+        var selector = 'html';                             //use body as selector if none provided
+        var searchTermRegEx = new RegExp(text,"ig");
         var matches = $(selector).text().match(searchTermRegEx);
         if(matches) {
             $('.highlighted').removeClass('highlighted');     //Remove old search highlights
                 $(selector).html($(selector).html()
-                    .replace(searchTermRegEx, "<span class='highlighted'>"+searchTerm+"</span>"));
+                    .replace(searchTermRegEx, "<span class='highlighted'>"+ text +"</span>"));
             if($('.highlighted:first').length) {             //if match found, scroll to where the first one appears
                 $(window).scrollTop($('.highlighted:first').position().top);
             }
             return true;
         }
-    }
+    
     return false;
-}
+
 });
